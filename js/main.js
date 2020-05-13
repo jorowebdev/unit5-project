f9d7a99ec54434d8d64633ac65e374a5a481b1e1
jorowebdev <26332224+jorowebdev@users.noreply.github.com>
2020-05-13 00:24:54 -0700

    Add for loop thru captions
    Add for loop that captures the image captions, in the lightbox data-title attribute, and logs the captions to the console

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 
-const searchBar = document.querySelector('.searchbar--input')
+const searchBar = document.querySelector('.searchbar--input');
+const galleryThumbs = document.querySelectorAll('.gallery--thumbs');
 
 searchBar.addEventListener('keyup', () => {
   const searchBarValue = searchBar.value.toLowerCase();
+  console.log(searchBarValue);
 });
+
+for (let i = 0; i < galleryThumbs.length; i += 1) {
+  const galleryCaption = galleryThumbs[i].getAttribute('data-title');
+  console.log(galleryCaption);
+}
 
 // lightbox options
 lightbox.option({
   'disableScrolling': true,
   'wrapAround': true
-})
+});
