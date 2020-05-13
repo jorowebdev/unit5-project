722e9aa6063ed837c35d6da1d882c069c122c2cc
jorowebdev <26332224+jorowebdev@users.noreply.github.com>
2020-05-13 01:47:55 -0700

    Change data-lightbox attr value
    Changed the data-lightbox attr value to gallery--landscape-set so that it wasn't the same value as the img class gallery--images

--- js/main.js
+++ js/main.js
@@ -13,13 +13,13 @@ searchBar.addEventListener('keyup', () => {
     if (galleryCaption.toLowerCase().includes(searchBarValue)) {
       // If true, the element thumbnail is displayed and the linked image is visible in the lightbox
       galleryThumbs[i].style.display = '';
-      galleryThumbs[i].setAttribute('data-lightbox', 'gallery--images');
+      galleryThumbs[i].setAttribute('data-lightbox', 'gallery--landscape-set');
     } else {
       // Else, look to see if the image alt attr contains the string saved to searchBarValue
       if (galleryImages[i].getAttribute('alt').toLowerCase().includes(searchBarValue)) {
         // If true, the element thumbnail is displayed and the linked image is visible in the lightbox
         galleryThumbs[i].style.display = '';
-        galleryThumbs[i].setAttribute('data-lightbox', 'gallery--images');
+        galleryThumbs[i].setAttribute('data-lightbox', 'gallery--landscape-set');
       } else {
         // Else, hide the thumbnail and hide the image from displaying in the lightbox
         galleryThumbs[i].style.display = 'none';
