65e607798786546abe83af71cb0d0784f0d2acc8
jorowebdev <26332224+jorowebdev@users.noreply.github.com>
2020-05-13 01:14:32 -0700

    Add if else statement to filter images
    Add if else statement that displays thumbnails and images with captions that includes input from the searchBar, and hides thumbnails and images that don't

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
 
 const searchBar = document.querySelector('.searchbar--input');
 const galleryThumbs = document.querySelectorAll('.gallery--thumbs');
+const galleryImages = document.querySelectorAll('.gallery--images')
 
 searchBar.addEventListener('keyup', () => {
   const searchBarValue = searchBar.value.toLowerCase();
   console.log(searchBarValue);
-});
 
-for (let i = 0; i < galleryThumbs.length; i += 1) {
-  const galleryCaption = galleryThumbs[i].getAttribute('data-title');
-  console.log(galleryCaption);
-}
+  for (let i = 0; i < galleryThumbs.length; i += 1) {
+    const galleryCaption = galleryThumbs[i].getAttribute('data-title');
+
+    if (galleryCaption.includes(searchBarValue)) {
+      galleryThumbs[i].style.display = '';
+      galleryThumbs[i].setAttribute('data-lightbox', 'gallery--images');
+    } else {
+      galleryThumbs[i].style.display = 'none';
+      galleryThumbs[i].removeAttribute('data-lightbox');
+    }
+  }
+
+});
 
 // lightbox options
 lightbox.option({
